40c1f99767e4ae0826ce20552667f07d77d8d1dd
Reidi Zhupa <reidizhupa56@gmail.com>
2026-06-16 14:22:07 +0900

    fix

---
+++ app/3fes/page.tsx
@@ -0,0 +1,25 @@
+import Awards from "./components/Awards";
+import Competition from "./components/Competition";
+import Concept from "./components/Concept";
+import Hero from "./components/Hero";
+
+export default function Home() {
+    return (
+        <main className="min-h-screen bg-neutral-900 text-white font-sans">
+            {/* メインビジュアル */}
+            <Hero />
+
+            {/* コンテンツエリア */}
+            <div className="max-w-4xl mx-auto px-4 py-12 space-y-24">
+                {/* 絆・コンセプト */}
+                <Concept />
+
+                {/* 競技について */}
+                <Competition />
+
+                {/* 賞について */}
+                <Awards />
+            </div>
+        </main>
+    );
+}
