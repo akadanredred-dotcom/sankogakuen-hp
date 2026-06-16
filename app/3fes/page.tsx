12545341b5885141c48d9ee63e9458fa20e363e6
te2boy23-bit <te2boy23@icloud.com>
2026-06-16 14:42:07 +0900

    ああ

--- app/3fes/page.tsx
+++ app/3fes/page.tsx
@@ -1,25 +1,29 @@
+import GoogleMap from "./components/GoogleMap";
 import Awards from "./components/Awards";
 import Competition from "./components/Competition";
 import Concept from "./components/Concept";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
 export default function Home() {
-    return (
-        <main className="min-h-screen bg-neutral-900 text-white font-sans">
-            {/* メインビジュアル */}
-            <Hero />
+  return (
+    <main className="min-h-screen bg-neutral-900 text-white font-sans">
+      {/* メインビジュアル */}
+      <Hero />
 
-            {/* コンテンツエリア */}
-            <div className="max-w-4xl mx-auto px-4 py-12 space-y-24">
-                {/* 絆・コンセプト */}
-                <Concept />
+      {/* コンテンツエリア */}
+      <div className="max-w-4xl mx-auto px-4 py-12 space-y-24">
+        {/* 絆・コンセプト */}
+        <Concept />
 
-                {/* 競技について */}
-                <Competition />
+        {/* 競技について */}
+        <Competition />
 
-                {/* 賞について */}
-                <Awards />
-            </div>
-        </main>
-    );
+        {/* 賞について */}
+        <Awards />
+        <GoogleMap />
+        <Footer />
+      </div>
+    </main>
+  );
 }
