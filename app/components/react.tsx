5f127f427a62efffbe9e41321c4bd25f83fd0508
te2boy23-bit <te2boy23@icloud.com>
2026-06-24 17:29:39 +0900

    ああ

--- app/components/react.tsx
+++ app/components/react.tsx
@@ -14,7 +14,7 @@ export default function AkadanPage() {
 
       <div className="absolute inset-0 bg-black/50 z-10" />
 
-      <div className="relative z-20 flex w-full h-full items-center justify-end pr-4 md:pr-[2%] pl-6">
+      <div className="relative z-20 flex w-full h-full items-center justify-end pr-4 md:pr-[2%] pl-6 pt-32">
         <div className="shrink-0 text-white flex flex-col justify-center items-start text-left">
           <div className="border-l-[32px] md:border-l-[48px] border-red-600 pl-8 md:pl-16 py-4">
             <h2 className="text-7xl md:text-[20rem] font-black mb-4 md:mb-8 tracking-[0.2em] leading-none drop-shadow-2xl">
