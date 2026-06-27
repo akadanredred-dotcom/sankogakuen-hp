b589f00403673341b9583b5a964194645589d858
te2boy23-bit <te2boy23@icloud.com>
2026-06-27 16:52:58 +0900

    count about 3fes

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -9,15 +9,37 @@ import Footer from "../3fes/components/Footer";
 
 export default function AwardsPage() {
     return (
-        <main className="w-full min-h-screen bg-[#F7F7F7] font-sans pb-24">
-            {/* ヘッダー部分 */}
-            <Navbar />
+        /* 
+          【調整ポイント】
+          - relative を追加：Countdown が最下部でピタッと止まるための基準点（親）になります。
+          - bg-[#F7F7F7] などの元のスタイルはそのまま維持しています。
+        */
+        <main className="relative w-full min-h-screen bg-[#F7F7F7] font-sans pb-24">
+            {/* 
+              ヘッダー部分 
+              ※もし AwardsHero の中にもすでに <Navbar /> が入っている場合は、
+              二重表示を防ぐためにここの <Navbar /> は削除するかコメントアウトしてください。
+            */}
+            {/* <Navbar /> */}
+            
             <AwardsHero />
 
             {/* 左右交互の賞紹介コンテンツ部分 */}
             <AwardsMain />
-            <Countdown />
-            <Footer />
+            
+            {/* 
+              💡 修正ポイント
+              フッター要素を検知できるように、外側を id="main-footer" を持った div で包みます。
+            */}
+            <div id="main-footer">
+                <Footer />
+            </div>
+
+            {/* 
+              💡 修正ポイント
+              カウントダウンコンポーネントに、上で指定したフッターのIDを渡します。
+            */}
+            <Countdown footerId="main-footer" />
         </main>
     );
 }
