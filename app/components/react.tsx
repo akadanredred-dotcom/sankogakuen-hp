f39723af68df8f5e1606494eb57e3bdad08a68de
te2boy23-bit <te2boy23@icloud.com>
2026-06-29 19:52:28 +0900

    三か条

--- app/components/react.tsx
+++ app/components/react.tsx
@@ -3,21 +3,16 @@ import Image from "next/image";
 
 export default function AkadanPage() {
   return (
-    <div className="relative w-full h-screen max-h-[86.6667vh] overflow-hidden font-sans">
+    <div className="relative w-full h-screen overflow-hidden font-sans">
       <Image
         src="/img/hyouga.jpg"
         alt="赤団 豹牙"
         fill
         priority
-        className="object-cover object-center "
+        className="object-cover object-center scale-110"
       />
 
-      <div className="absolute inset-0 bg-black/50 z-10" />
-
-      {/* 
-        全体の配置コンテナ 
-        md:pr-[12%] で右側の余白を少し広げ、よりバランスを取りやすくしています
-      */}
+      <div className="absolute inset-0 bg-black/30 z-10" />
     </div>
   );
 }
