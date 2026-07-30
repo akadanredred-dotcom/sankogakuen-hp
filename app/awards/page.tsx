19f2ffd940ceb504b94f48b3c513913aeefc221b
te2boy23-bit <282310403+te2boy23-bit@users.noreply.github.com>
2026-07-31 03:02:02 +0900

    fix

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import AwardsHero from "./components/Hero";
 import AwardsMain from "./components/Main";
 import Navbar from "../components/Navbar";
-import Countdown from "./components/Countdown";
+import Countdown from "../components/Countdown";
 import Footer from "../3fes/components/Footer";
 
 export default function AwardsPage() {
